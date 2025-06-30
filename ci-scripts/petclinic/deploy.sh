f752fad217877549c6b3f14f0c1bc79aa34151fd
BDanielaG <129323876+BDanielaG@users.noreply.github.com>
2025-06-30 19:11:06 +0300

    Update deploy.sh

--- ci-scripts/petclinic/deploy.sh
+++ ci-scripts/petclinic/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# This script deploys the built PetClinic JAR
 
-set -xe  # Exit on error, show commands
+
+set -xe 
 
 # Set global env
 TARGET_USER="daniela"
@@ -13,11 +13,10 @@ echo "-----------------------------------------------------"
 echo "--- Starting PetClinic Application ---"
 echo "-----------------------------------------------------"
 
-# Create working dir and move into it
 mkdir -p app
 cd app
 
-echo "Downloading artifact from Nexus..."
+
 response_status=$(curl -s -u "$NEXUS_USER:$NEXUS_PASSWORD" -O -w "%{http_code}" \
   "http://nexus:8081/repository/petclinic-project/${JAR_FILE_NAME}")
 
@@ -29,7 +28,7 @@ esac
 
 ls -la
 
-# Confirm JAR was downloaded
+
 if [ ! -f "$JAR_FILE_NAME" ]; then
     echo "ERROR: JAR file not found!"
     exit 1
@@ -37,17 +36,16 @@ fi
 
 echo "Starting the PetClinic application..."
 
-# Remove and Create directory on host machine
 ssh -i "$SSH_KEY_FILE" -o StrictHostKeyChecking=no "$TARGET_USER@$TARGET_HOST" \
 "rm -rf '$TARGET_APP_DIR' && mkdir -p '$TARGET_APP_DIR'"
 
 
-# Upload JAR to WSL
+
 scp -i "$SSH_KEY_FILE" -o StrictHostKeyChecking=no \
     "$JAR_FILE_NAME" \
     "$TARGET_USER@$TARGET_HOST:$TARGET_APP_DIR/"
 
-# Start app on remote WSL
+
 REMOTE_PID=$(ssh -i "$SSH_KEY_FILE" -o StrictHostKeyChecking=no "$TARGET_USER@$TARGET_HOST" <<EOF
 set -xe
 cd "$TARGET_APP_DIR"
